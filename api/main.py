17f13d7d7db0288b934dd02b07e6c7b35d561cbb
Jose López <jlopep09@estudiantes.unileon.es>
2026-01-27 18:54:36 +0100

    Created docker compose services

---
+++ api/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/status")
+async def root():
+    return {"message": "Status: Ok"}
---
+++ ui/constants.js
@@ -0,0 +1,1 @@
+export const VITE_APP_NAME = 'JLExpensia'
