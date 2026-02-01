198474b6b8b409050d013bd02989af00696dfea4
jlopep09 <jlopep09@estudiantes.unileon.es>
2026-02-01 10:32:04 +0100

    Fixed footer on small screens

--- ui/src/common/components/Footer.jsx
+++ ui/src/common/components/Footer.jsx
@@ -3,18 +3,21 @@ import { getYear } from '../utilities/time'
 import { VITE_APP_NAME } from '../../../constants'
 const Footer = () => {
   return (
-    <footer className="mt-16 border-t border-base-200 py-8">
-        <div className="container mx-auto px-6 md:px-12 flex flex-col md:flex-row items-center justify-between gap-6">
-          <div className="flex items-center gap-3">
+    <footer className="mt-16 border-t border-base-200 py-8 w-full">
+        <div className="w-full container mx-auto px-6 md:px-12 flex flex-col md:flex-row items-center justify-between gap-6">
+          <div className="flex items-center gap-3 w-full justify-between">
+            <div className='flex items-center gap-3'>
+
             <div className="w-8 h-8 rounded bg-linear-to-br from-primary to-slate-500 flex items-center justify-center text-white font-bold">JL</div>
           
             <div>
               <div className="font-semibold">{VITE_APP_NAME}</div>
               <div className="text-xs opacity-70">© {new Date().getFullYear()} {VITE_APP_NAME}</div>
             </div>
+            </div>
+          <div className="text-sm opacity-80"><span className="text-primary hover:text-accent"><a href="https://joselp.com/" className='text-nowrap' target="_blank">José Antonio López Pérez</a></span></div>
           </div>
 
-          <div className="text-sm opacity-80"><span className="text-primary hover:text-accent"><a href="https://joselp.com/" target="_blank">José Antonio López Pérez</a></span></div>
         </div>
       </footer>
       )
--- ui/src/routes/Home/Home.jsx
+++ ui/src/routes/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Hello world</div>
+    <div className='w-full'>Hello world</div>
   )
 }
 
